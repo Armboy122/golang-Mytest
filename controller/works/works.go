ad999c8ff2f0cfe25f265e8a1b21fd6ead730b95
Armboy122 <arm.09686@gmail.com>
2023-06-03 23:13:29 +0700

    final

--- controller/works/works.go
+++ controller/works/works.go
@@ -9,11 +9,59 @@ import (
 
 func Creatework(c *gin.Context) {
 	var json orm.Works // json เก็บข้อมูลของสิ่งที่ส่งเข้าไป
+	c.Bind(&json)
 
-	if err := c.ShouldBindJSON(&json); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+	result := orm.Db.Create(&json) //กำหนดค่าไว้เช็ค err
+
+	if result.Error != nil {
+		c.Status(400)
+		return
 	}
 
-	orm.Db.Create(&json)
-	c.JSON(http.StatusOK, gin.H{"data": json})
+	c.JSON(http.StatusOK, gin.H{
+		"post": json,
+	})
+
+}
+
+func Readworks(c *gin.Context) {
+	var json []orm.Works // json เก็บข้อมูลของDB เป็นแบบ array
+	orm.Db.Find(&json)
+	c.JSON(http.StatusOK, gin.H{
+		"status":  "ok",
+		"message": "Works Read success",
+		"users":   json,
+	})
+}
+
+func Listworks(c *gin.Context) {
+	id := c.Param("id") // รับค่า id ที่ส่งมา
+	var json orm.Works  // json เก็บข้อมูลของสิ่งที่ส่งเข้าไป
+	orm.Db.First(&json, id)
+	c.JSON(http.StatusOK, gin.H{
+		"status":  "ok",
+		"message": "Works Read success",
+		"users":   json,
+	})
+
+}
+func Deletworks(c *gin.Context) {
+	id := c.Param("id") // รับค่า id ที่ส่งมา
+	var json orm.Works  // json เก็บข้อมูลของสิ่งที่ส่งเข้าไป
+	orm.Db.Delete(&json, id)
+	c.JSON(http.StatusOK, gin.H{
+		"status":  "ok",
+		"message": "Delete success",
+	})
+}
+func Updatework(c *gin.Context) {
+	id := c.Param("id")                    // รับค่า id ที่ส่งมา
+	var json orm.Works                     // ตัวแปรข้อมูลใน DB
+	var work orm.Works                     // ตัวแปรข้อมูลใหม่ที่จะใส่ค่าเข้าไป
+	c.Bind(&work)                          //เช็คค่าความต่างของ ข้อมูลใหม่เก่า
+	orm.Db.First(&json, id)                //หาข้อมูล id จาก DB
+	orm.Db.Model(&json).Updates(orm.Works{ // update ข้อมูลใส่เข้าไป
+		Date:   work.Date,
+		Detail: work.Detail,
+	})
 }
--- main.go
+++ main.go
@@ -22,15 +22,20 @@ func main() {
 	}
 	//---------------------------ต่อ db-----------------------------
 	orm.InitDB()
-	//---------------------------path ต่างๆของDB-----------------------------
+	//---------------------------path ต่างๆของLogin-----------------------------
 	r := gin.Default()
 	r.Use(cors.Default())
 	r.POST("/register", auth.Register) // สมัครใช้งาน
 	r.POST("/login", auth.Login)       // login เข้าระบบ
-	r.POST("/creat", works.Creatework) // login เข้าระบบ
 	authorized := r.Group("/user", middleware.Logger())
 	authorized.GET("/readall", user.ReadAll) // สำหรับแอดมินไว้ดูงานทุกคน
 	authorized.GET("/profile", user.Profile) // ให้ User ดูงานและแก้ไขห้ามลบ
+	//---------------------------path ต่างๆของCRUD-----------------------------
+	r.POST("/work", works.Creatework)       // ลงข้อมูลงาน
+	r.GET("/work", works.Readworks)         // ดูข้อมูลงานทั้งหมด
+	r.GET("/work/:id", works.Listworks)     // ดูข้อมูลงานแต่ละ ID
+	r.PUT("/work/:id", works.Updatework)    // แก้ไข้ข้อมูล
+	r.DELETE("/work/:id", works.Deletworks) // ลบข้อมูลงานแต่ละ ID
 
 	r.Run("localhost:8000") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
